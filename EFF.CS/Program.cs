aa52feea805d6f66744069638d84cb2ee35cb707
sp <some@gmail.com>
2015-08-29 02:21:09 +0600

    done

---
+++ EFF.CS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+
+namespace EFF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"D:\dev\EntityFramework";
+
+            var excluded = new[] { @"\obj\Debug", @"\bin\Debug", @"\Properties" };
+            Func<string, bool> isIncluded = (name) => excluded.All(e => !name.Contains(e));
+            Func<DirectoryInfo, bool> witoutFiles = (di) => !di.GetFiles().Any();
+            Func<DirectoryInfo, bool> witoutSubDir = (di) => !di.GetDirectories().Any();
+
+            var dirs = new DirectoryInfo(path)
+                .GetDirectories("*", SearchOption.AllDirectories)
+                .Where(witoutFiles)
+                .Where(witoutSubDir)
+                .Select(di => di.FullName)
+                .Where(isIncluded);
+
+            dirs.ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
---
+++ EFF/Script.ps1
@@ -0,0 +1,14 @@
+﻿$path = "D:\dev\EntityFramework"
+
+function IsIncluded($name)
+{
+  $exclude = "*\obj\Debug*", "*\bin\Debug*", "*\Properties"
+  $mathes = $exclude | Where-Object {$name -like $_}
+  $mathes.Count -eq 0
+}
+
+Get-ChildItem $path -Directory -Recurse `
+ | Where-Object {$_.GetDirectories().Count -eq 0} `
+ | Where-Object {$_.GetFiles().Count -eq 0} `
+ | Where-Object {IsIncluded $_.FullName} `
+ | Format-Wide -Property FullName -Column 1
